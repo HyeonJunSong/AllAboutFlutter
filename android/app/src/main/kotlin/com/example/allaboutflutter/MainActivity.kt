7557bb5f12fd8039e2aed9c9d5ff0ba1064faf96
HyeonJunSong <music9801@naver.com>
2022-09-23 14:30:19 +0900

    initial commit

---
+++ android/app/src/main/kotlin/com/example/allaboutflutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.allaboutflutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Home(),
+    );
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+      ),
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: (){},
+            child: Text("Row&Col")
+          ),
+        ],
+      ),
+    );
+  }
+}
